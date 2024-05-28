17ea4d495082eb1c2b8a91445ab802507191ff95
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-05-28 12:15:22 +0200

    implementing TwoPhaseMoveState to the PuzzleState Class

--- src/main/java/swapstones/model/PuzzleState.java
+++ src/main/java/swapstones/model/PuzzleState.java
@@ -1,4 +1,37 @@
 package swapstones.model;
 
-public class PuzzleState {
+import puzzle.TwoPhaseMoveState;
+
+import java.util.Set;
+
+public class PuzzleState implements TwoPhaseMoveState<Integer> {
+    @Override
+    public boolean isLegalToMoveFrom(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+
+    @Override
+    public boolean isLegalMove(TwoPhaseMove<Integer> integerTwoPhaseMove) {
+        return false;
+    }
+
+    @Override
+    public void makeMove(TwoPhaseMove<Integer> integerTwoPhaseMove) {
+
+    }
+
+    @Override
+    public Set<TwoPhaseMove<Integer>> getLegalMoves() {
+        return Set.of();
+    }
+
+    @Override
+    public TwoPhaseMoveState<Integer> clone() {
+        return null;
+    }
 }
