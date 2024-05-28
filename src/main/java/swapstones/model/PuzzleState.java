e7bf7e37e198ac097cab35c241f6f965e91bd5d0
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-05-28 13:39:39 +0200

    implementing body of the methods without the clone method in the PuzzleState Class

--- src/main/java/swapstones/model/PuzzleState.java
+++ src/main/java/swapstones/model/PuzzleState.java
@@ -1,33 +1,104 @@
 package swapstones.model;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import puzzle.TwoPhaseMoveState;
 
-import java.util.Set;
+import java.util.*;
 
 public class PuzzleState implements TwoPhaseMoveState<Integer> {
-    @Override
-    public boolean isLegalToMoveFrom(Integer integer) {
-        return false;
+    public static final int BOARD_SIZE = 16;
+
+    private ReadOnlyObjectWrapper<Stone>[] board = new ReadOnlyObjectWrapper[BOARD_SIZE];
+
+
+    public PuzzleState() {
+        int redCount = 0;
+        int blackCount = 0;
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            if (redCount < 3 && (i % 2 == 0)) {
+                board[i] = new ReadOnlyObjectWrapper<>(Stone.TAIL);
+                redCount++;
+            } else if (blackCount < 3 && (i % 2 != 0)) {
+                board[i] = new ReadOnlyObjectWrapper<>(Stone.HEAD);
+                blackCount++;
+            } else {
+                board[i] = new ReadOnlyObjectWrapper<>(Stone.EMPTY);
+            }
+        }
     }
+
+    @Override
+    public String toString() {
+        return "PuzzleState{" +
+                "board=" + Arrays.toString(board) +
+                '}';
+    }
+
+    @Override
+    public boolean isLegalToMoveFrom(Integer fromIndex) {
+        return fromIndex >= 0 && fromIndex <= BOARD_SIZE - 2
+                && board[fromIndex].get() != Stone.EMPTY
+                && board[fromIndex + 1].get() != Stone.EMPTY;
+    }
+
+    public boolean isLegalToMoveTo(Integer toIndex) {
+        return toIndex >= 0 && toIndex <= BOARD_SIZE - 2
+                && board[toIndex].get() == Stone.EMPTY
+                && board[toIndex + 1].get() == Stone.EMPTY;
+    }
+
 
     @Override
     public boolean isSolved() {
+        for (var i = 0; i < 11; i++) {
+            if ((board[i].get() == Stone.TAIL && board[i + 1].get() == Stone.TAIL && board[i + 2].get() == Stone.TAIL)
+                    && (board[i + 3].get() == Stone.HEAD && board[i + 4].get() == Stone.HEAD && board[i + 5].get() == Stone.HEAD)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean isLegalMove(TwoPhaseMove<Integer> integerTwoPhaseMove) {
-        return false;
+    public boolean isLegalMove(TwoPhaseMove<Integer> directionTwoPhaseMove) {
+        int fromIndex = directionTwoPhaseMove.from();
+        int toIndex = directionTwoPhaseMove.to();
+
+        return isLegalToMoveFrom(fromIndex)
+                && isLegalToMoveTo(toIndex);
     }
 
     @Override
-    public void makeMove(TwoPhaseMove<Integer> integerTwoPhaseMove) {
-
+    public void makeMove(TwoPhaseMove<Integer> directionTwoPhaseMove) {
+        if (!isLegalMove(directionTwoPhaseMove)) {
+            throw new IllegalArgumentException("Illegal move: " + directionTwoPhaseMove);
+        }
+        Stone source[] = new Stone[2];
+        source[0] = board[directionTwoPhaseMove.from()].get();
+        source[1] = board[directionTwoPhaseMove.from() + 1].get();
+        board[directionTwoPhaseMove.from()].set(Stone.EMPTY);
+        board[directionTwoPhaseMove.from() + 1].set(Stone.EMPTY);
+        board[directionTwoPhaseMove.to()].set(source[0]);
+        board[directionTwoPhaseMove.to() + 1].set(source[1]);
     }
 
     @Override
     public Set<TwoPhaseMove<Integer>> getLegalMoves() {
-        return Set.of();
+        Set<TwoPhaseMove<Integer>> legalMoves = new HashSet<>();
+
+        for (int i = 0; i < BOARD_SIZE - 1; i++) {
+            for (int j = BOARD_SIZE - 1; j > 0; j--) {
+                if (board[i].get() != Stone.EMPTY && board[i + 1].get() != Stone.EMPTY) {
+                    if (board[j].get() == Stone.EMPTY && board[j-1].get() == Stone.EMPTY) {
+                        TwoPhaseMove<Integer> move = new TwoPhaseMove<>(i,j);
+                        legalMoves.add(move);
+                    }
+                }
+            }
+
+        }
+
+        return legalMoves;
     }
 
     @Override
