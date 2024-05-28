dadc325ac9db8fb4c7e7f0cab19a3e781516e561
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-05-28 13:47:58 +0200

    adding Main class and Game Application for swapstones.game package and fxml file to recources package

---
+++ src/main/java/swapstones/game/Main.java
@@ -0,0 +1,10 @@
+package swapstones.game;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Application.launch(GameApplication.class, args);
+    }
+}
